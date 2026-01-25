500139bd1f24cdb716b8f5635128a7c5bf739043
Minahil Ismail <minahilismail44@gmail.com>
2026-01-25 21:57:56 +0500

    fix: icons path

--- projects/brickclay-lib/src/lib/assets/icons.ts
+++ projects/brickclay-lib/src/lib/assets/icons.ts
@@ -1,8 +1,8 @@
 // Icon paths that will be resolved relative to the library's assets folder
 // When published to npm, users will need to configure their angular.json to include these assets
 export const BrickclayIcons = {
-  arrowleft: 'assets/brickclay-lib/icons/chevron-left.svg',
-  arrowRight: 'assets/brickclay-lib/icons/chevron-right.svg',
-  calenderIcon: 'assets/brickclay-lib/icons/calender.svg',
-  timerIcon: 'assets/brickclay-lib/icons/timer.svg',
+  arrowleft: 'assets/icons/chevron-left.svg',
+  arrowRight: 'assets/icons/chevron-right.svg',
+  calenderIcon: 'assets/icons/calender.svg',
+  timerIcon: 'assets/icons/timer.svg',
 } as const;
